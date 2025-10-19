f8c6f518ab92286d92cfdb1852652c9fd0793cfc
Franco Colmenarez <info@francoacg.com>
2025-10-18 21:23:13 -0500

    initial shuffle implementation

--- lib/widgets/player/buttons/shuffle.dart
+++ lib/widgets/player/buttons/shuffle.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:music_player/core/player/enums.dart';
+import 'package:music_player/core/player/player_controller.dart';
+import 'package:music_player/providers/player_controller_provider.dart';
 
 class ShuffleButton extends ConsumerWidget {
   const ShuffleButton({super.key});
 
+  IconData _getIcon(PlayerController playerController) {
+    final iconMap = {
+      ShuffleMode.sequential: Icons.queue_music,
+      ShuffleMode.random: Icons.shuffle,
+      ShuffleMode.randomBasedOnHistory: Icons.history,
+    };
+    return iconMap[playerController.shuffleMode]!;
+  }
+
+  String _getTooltop(PlayerController playerController) {
+    final shuffleModeMap = {
+      ShuffleMode.sequential: "Shuffle disabled",
+      ShuffleMode.random: "Randomized",
+      ShuffleMode.randomBasedOnHistory: "Randomized special",
+    };
+    return shuffleModeMap[playerController.shuffleMode]!;
+  }
+
+  void _toggleShuffleMode(PlayerControllerNotifier playerControllerNotifier) {
+    playerControllerNotifier.toggleShuffleMode();
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final playerController = ref.watch(playerControllerProvider);
+    final playerControllerNotifier = ref.read(playerControllerProvider.notifier);
     return IconButton(
-      icon: const Icon(Icons.shuffle),
-      tooltip: 'Shuffle mode',
+      icon: Icon(_getIcon(playerController)),
+      tooltip: _getTooltop(playerController),
       onPressed: () async {
-        print("clicked on shuffle mode");
+        _toggleShuffleMode(playerControllerNotifier);
       },
     );
   }
