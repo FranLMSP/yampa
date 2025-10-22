156251e2b08c63005b309be31dfbe945136cda00
Franco Colmenarez <info@francoacg.com>
2025-10-21 22:55:21 -0500

    show mini player only on tabs other than main player

--- lib/widgets/main_browser/main.dart
+++ lib/widgets/main_browser/main.dart
@@ -7,7 +7,7 @@ import 'package:yampa/widgets/player/big_player.dart';
 import 'package:yampa/widgets/player/mini_player.dart';
 import 'package:yampa/widgets/utils.dart';
 
-class MainBrowser extends ConsumerWidget {
+class MainBrowser extends ConsumerStatefulWidget {
 
   const MainBrowser({
     super.key,
@@ -15,6 +15,18 @@ class MainBrowser extends ConsumerWidget {
   });
 
   final ViewMode viewMode;
+
+  @override
+  ConsumerState<MainBrowser> createState() => _MainBrowserState();
+}
+
+class _MainBrowserState extends ConsumerState<MainBrowser> {
+  bool _showMiniPlayer = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   List<Widget> _getTabs() {
     final tabs = [
@@ -24,7 +36,7 @@ class MainBrowser extends ConsumerWidget {
       Tab(icon: Icon(Icons.settings), text: "Settings"),
     ];
 
-    if (viewMode == ViewMode.portrait) {
+    if (widget.viewMode == ViewMode.portrait) {
       tabs.insert(0, Tab(icon: Icon(Icons.play_arrow), text: "Player"));
     }
 
@@ -39,7 +51,7 @@ class MainBrowser extends ConsumerWidget {
       Icon(Icons.settings),
     ];
 
-    if (viewMode == ViewMode.portrait) {
+    if (widget.viewMode == ViewMode.portrait) {
       elements.insert(0, BigPlayer());
     }
 
@@ -47,15 +59,29 @@ class MainBrowser extends ConsumerWidget {
   }
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final tabs = _getTabs();
     final elements = _getTabsContent();
     return DefaultTabController(
       length: tabs.length,
       child: Scaffold(
-        appBar: TabBar(tabs: tabs),
+        appBar: TabBar(
+          tabs: tabs,
+          onTap: (index) {
+            if (index != 0 && widget.viewMode == ViewMode.portrait && _showMiniPlayer == false) {
+              setState(() {
+                _showMiniPlayer = true;
+              });
+            }
+            if ((index == 0 || widget.viewMode != ViewMode.portrait) && _showMiniPlayer == true) {
+              setState(() {
+                _showMiniPlayer = false;
+              });
+            }
+          },
+        ),
         body: TabBarView(children: elements),
-        bottomSheet: MiniPlayer(),
+        bottomSheet: _showMiniPlayer ? MiniPlayer() : null,
       ),
     );
   }
