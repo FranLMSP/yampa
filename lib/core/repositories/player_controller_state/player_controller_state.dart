a1189e884299ea2f9ab61308babebd9bc6f28188
Franco Colmenarez <info@francoacg.com>
2025-11-11 23:26:11 -0500

    wip player controller state repository

---
+++ lib/core/repositories/player_controller_state/player_controller_state.dart
@@ -0,0 +1,7 @@
+import 'package:yampa/models/player_controller_state.dart';
+
+abstract class PlayerControllerStateRepository {
+  Future<LastPlayerControllerState> getPlayerControllerState();
+  Future<void> savePlayerControllerState(LastPlayerControllerState playerControllerState);
+  Future<void> close();
+}
