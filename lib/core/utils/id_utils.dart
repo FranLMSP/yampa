1ea4cdddc991ff1506233d7b3edd0daf1e6f45ba
Franco Colmenarez <info@francoacg.com>
2025-11-17 20:08:40 -0500

    await computeFileChecksum

--- lib/core/utils/id_utils.dart
+++ lib/core/utils/id_utils.dart
@@ -2,5 +2,5 @@ import 'package:crypto/crypto.dart';
 import 'package:yampa/core/utils/checksum_utils.dart';
 
 Future<String> generateTrackId(String filePath) async {
-  return computeFileChecksum(filePath, sha256.convert);
+  return await computeFileChecksum(filePath, sha256.convert);
 }
