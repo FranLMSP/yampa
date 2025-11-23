1a545ddd423c15d074a3af31498eee183d34fdbc
Franco Colmenarez <info@francoacg.com>
2025-11-23 13:21:27 -0500

    Use a different ID algorithm

--- lib/core/utils/id_utils.dart
+++ lib/core/utils/id_utils.dart
@@ -1,7 +1,9 @@
 // import 'package:crypto/crypto.dart';
+// import 'package:flutter/foundation.dart';
 import 'package:yampa/core/utils/checksum_utils.dart';
 
 Future<String> generateTrackId(String filePath) async {
-  // return await computeFileChecksum(filePath, sha256.convert);
-  return await computeFastFileFingerprint(filePath);
+  // return await compute(computeFileChecksum, filePath);
+  // return await computeFastFileFingerprint(filePath);
+  return computeFastFileFingerprintV2(filePath);
 }
